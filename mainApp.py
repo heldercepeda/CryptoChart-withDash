caf314741bfda031639c59431c0883ecf5ec88db
Helder <helder_cepeda@hotmail.com>
2020-07-21 09:57:58 +0100

    Small changes

--- mainApp.py
+++ mainApp.py
@@ -4,11 +4,9 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(
-    __name__,
-    external_stylesheets=external_stylesheets
+    __name__
                 )
 
 df = pd.DataFrame({
