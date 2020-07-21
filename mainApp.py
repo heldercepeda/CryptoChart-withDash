aae088fb9cab643e284700de99bb1e27b9376152
Helder <helder_cepeda@hotmail.com>
2020-07-21 10:59:36 +0100

    Small changes

--- mainApp.py
+++ mainApp.py
@@ -1,34 +1,64 @@
+# local imports
+from assets.page_layout import index_string, main_layout, dropdowns
+
+# imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objects as go
 
 
-app = dash.Dash(
-    __name__
-                )
+app = dash.Dash(__name__)
+app.index_string = index_string
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+app.layout = html.Div(
+    children=main_layout(
+        controls=dropdowns,
+        content=dcc.Loading(
+            type="dot",
+            children=html.Div(
+                children=[
+                    dcc.Interval(
+                        id='interval',
+                        interval=10*1000,  # in milliseconds
+                        n_intervals=0
+                    ),
+                    dcc.Graph(
+                        id='example-graph',
+                        figure=go.Figure(data=[]).update_layout(
+                            margin={'l': 20, 'b': 30, 't': 10, 'r': 20},
+                            template="plotly_dark",
+                            height=500,
+                            plot_bgcolor='black',
+                            paper_bgcolor='black',
+                            annotations=[
+                                {
+                                    "text": "No data to show!<br>Please select a market",
+                                    "xref": "paper",
+                                    "yref": "paper",
+                                    "showarrow": False,
+                                    "font": {
+                                        "size": 18,
+                                        "color": "gray"
+                                    }
+                                }
+                            ],
+                            xaxis={
+                                "visible": False
+                            },
+                            yaxis={
+                                "visible": False
+                            }
+                        ),
+                        className='mx-2 mt-4'
+                    )
+                ]
+            )
+        ),
+        footer=None
     )
-])
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
