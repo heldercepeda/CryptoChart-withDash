16b3f036e3641db830adc651ad992e403a951e63
Helder <helder_cepeda@hotmail.com>
2020-07-21 11:19:25 +0100

    callbacks added to refresh pairs when a market is selected

--- mainApp.py
+++ mainApp.py
@@ -1,11 +1,14 @@
 # local imports
 from assets.page_layout import index_string, main_layout, dropdowns
+from assets.get_data import pairs_list
 
 # imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 
 app = dash.Dash(__name__)
@@ -21,7 +24,7 @@ app.layout = html.Div(
                 children=[
                     dcc.Interval(
                         id='interval',
-                        interval=10*1000,  # in milliseconds
+                        interval=60*1000,  # in milliseconds
                         n_intervals=0
                     ),
                     dcc.Graph(
@@ -59,6 +62,38 @@ app.layout = html.Div(
         footer=None
     )
 )
+
+
+# Adjust the refresh rate according to the period
+@app.callback(
+    Output("interval", "interval"),
+    [
+        Input("period", "value")
+    ]
+)
+def update_interval(period):
+    if not period:
+        raise PreventUpdate
+    else:
+        return period*1000
+
+
+# Update pairs when a market is selected
+@app.callback(
+    [
+        Output("pairs", "options"),
+        Output("pairs", "disabled")
+    ],
+    [
+        Input("markets", "value")
+    ]
+)
+def update_pairs(market):
+    if not market:
+        raise PreventUpdate
+    else:
+        return pairs_list(market), False
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
