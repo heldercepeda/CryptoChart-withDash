1c29ad948566234f50963259f7651cb8482a88da
Helder <helder_cepeda@hotmail.com>
2020-07-21 11:40:37 +0100

    OHLC function added

--- assets/get_data.py
+++ assets/get_data.py
@@ -48,3 +48,37 @@ def pairs_list(market):
         for i in df.loc[df.active].index  # We just want to display the pairs that are active on the given market
     ]
     return list_pairs
+
+
+# get data for OHLC chart
+def ohlc(market, pair, before=None, after=None, periods=None):
+    url = fr"https://api.cryptowat.ch/markets/{market}/{pair}/ohlc"
+    params = {
+        "apikey": api_key,
+        "before": before,  # Unix timestamp. Only return candles opening before this time. Example: 1481663244
+        "after": after,  # Unix timestamp. Only return candles opening after this time. Example 1481663244
+        "periods": periods  # comma separated integers. Only return these time periods.
+    }
+    result = requests.get(url, params=params).json()
+    if "result" not in result.keys():
+        return pd.DataFrame()
+    list_ = []
+    values = [60, 180, 300, 900, 1800, 3600, 7200, 14400, 21600, 43200, 86400, 259200, 604800]
+    labels = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "12h", "1d", "3d", "1w"]
+    new_keys = []
+    for key in result["result"].keys():
+        try:
+            new_keys.append(int(key))
+        except:
+            pass
+    for key in new_keys:
+        df = pd.DataFrame(result["result"][str(key)],
+                          columns=["CloseTime", "OpenPrice", "HighPrice", "LowPrice", "ClosePrice", "Volume",
+                                   "QuoteVolume"])
+        df["Period"] = [key for _ in df.index]
+        ii = values.index(key)
+        df["Label"] = [labels[ii] for _ in df.index]
+        list_.append(df)
+    final_df = pd.concat(list_)
+    final_df = final_df.sort_values(["Period", "CloseTime"])
+    return final_df
