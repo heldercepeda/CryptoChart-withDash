7ed5f1628154799622ee130fa496e319b2556c77
Helder <helder_cepeda@hotmail.com>
2020-07-21 09:31:11 +0100

    Function pairs_list added

--- assets/get_data.py
+++ assets/get_data.py
@@ -26,3 +26,25 @@ def market_list():
         for i in df.loc[df.active].index  # We just want to display the markets that are active
     ]
     return final
+
+
+# get a list of pairs for the selected market
+# This will be used on the dropdown menu
+# The function will return a list of dicts
+
+
+def pairs_list(market):
+    url = fr"https://api.cryptowat.ch/markets/{market}"
+    params = {
+        "apikey": api_key
+    }
+    result = requests.get(url, params=params).json()
+    df = pd.DataFrame(result["result"]).sort_values("pair")
+    final = [
+        {
+            'label': df.pair[i].upper(),
+            'value': df.pair[i]
+        }
+        for i in df.loc[df.active].index  # We just want to display the pairs that are active on the given market
+    ]
+    return final
